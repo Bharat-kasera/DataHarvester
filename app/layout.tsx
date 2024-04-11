7b593f3e1ae26c4ff684056bbc0ac548f35be208
Bharat Kasera <bharat.codewolf@gmail.com>
2024-04-11 21:55:47 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'], 
@@ -21,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
           <Navbar />
